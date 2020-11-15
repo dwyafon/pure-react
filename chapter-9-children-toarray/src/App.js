5c205f3ca281584f53d2c11a9537687df91d76b2
Shaw Malcolm <shawmalcolm@protonmail.com>
2020-11-15 09:27:05 +0000

    finish toarrray components

--- chapter-9-children-toarray/src/App.js
+++ chapter-9-children-toarray/src/App.js
@@ -2,18 +2,52 @@ import React from "react";
 import "./App.css";
 // import PropTypes from 'prop-types';
 
-const App = () => <ErrorBox>Error: System limit for number of file watchers reached</ErrorBox>;
-
-//generic component
-const ErrorBox = ({ children }) => {
+const App = () => {
   return (
-    <div className="container">
-      <span className="message">
-        <i className="fas fa-exclamation-circle fa-2x" />
-        {children}
-      </span>
-    </div>
+    <>
+      <FirstChildOnly>
+        <li>A</li>
+        <li>B</li>
+        <li>C</li>
+      </FirstChildOnly>
+      <LastChildOnly>
+        <li>D</li>
+        <li>E</li>
+        <li>F</li>
+      </LastChildOnly>
+      <Head number={2}>
+        <li>G</li>
+        <li>H</li>
+        <li>I</li>
+      </Head>
+      <Tail number={2}>
+        <li>J</li>
+        <li>K</li>
+        <li>L</li>
+      </Tail>
+    </>
   );
 };
 
+const FirstChildOnly = (props) => {
+  const firstChildArr = React.Children.toArray(props.children);
+  return <ul>{firstChildArr[0]}</ul>;
+};
+const LastChildOnly = (props) => {
+  const lastChildArr = React.Children.toArray(props.children);
+  return <ul>{lastChildArr[2]}</ul>;
+};
+const Head = (props) => {
+  const number = props.number;
+  const headArr = React.Children.toArray(props.children);
+  const firstTwo = headArr.slice(0, number)
+  return <ul>{firstTwo}</ul>;
+};
+const Tail = (props) => {
+  const number = props.number;
+  const tailArr = React.Children.toArray(props.children);
+  const lastOne = tailArr.slice(number);
+  return ( <ul>{lastOne}</ul>
+  )
+};
 export default App;
