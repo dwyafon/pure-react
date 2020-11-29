4a1a79f973dd805766afdf756c5a93903119e084
Shaw Malcolm <shawmalcolm@protonmail.com>
2020-11-29 08:41:12 +0000

    update grid styling

--- chapter-10-old-school-pinterest-dashboard/src/objects/cards.js
+++ chapter-10-old-school-pinterest-dashboard/src/objects/cards.js
@@ -3,55 +3,91 @@ const cards = [
         id: 1,
         cardTitle: 'Title Here',
         heading: 'Heading Here',
-        mainImage: 'https://source.unsplash.com/500x500/?danmark',
-        subImage1: 'https://source.unsplash.com/150x150/?danmark',
-        subImage2: 'https://source.unsplash.com/150x150/?danmark',
-        subImage3: 'https://source.unsplash.com/150x150/?danmark'
+        mainImage: 'https://source.unsplash.com/300x200/?danmark',
+        subImage1: 'https://source.unsplash.com/90x90/?danmark',
+        subImage2: 'https://source.unsplash.com/90x90/?danmark',
+        subImage3: 'https://source.unsplash.com/90x90/?danmark'
     },
     {
         id: 2,
         cardTitle: 'Title Here',
         heading: 'Heading Here',
-        mainImage: 'https://source.unsplash.com/500x500/?danmark',
-        subImage1: 'https://source.unsplash.com/150x150/?danmark',
-        subImage2: 'https://source.unsplash.com/150x150/?danmark',
-        subImage3: 'https://source.unsplash.com/150x150/?danmark'
+        mainImage: 'https://source.unsplash.com/300x200/?danmark',
+        subImage1: 'https://source.unsplash.com/90x90/?danmark',
+        subImage2: 'https://source.unsplash.com/90x90/?danmark',
+        subImage3: 'https://source.unsplash.com/90x90/?danmark'
     },
     {
         id: 3,
         cardTitle: 'Title Here',
         heading: 'Heading Here',
-        mainImage: 'https://source.unsplash.com/500x500/?danmark',
-        subImage1: 'https://source.unsplash.com/150x150/?danmark',
-        subImage2: 'https://source.unsplash.com/150x150/?danmark',
-        subImage3: 'https://source.unsplash.com/150x150/?danmark'
+        mainImage: 'https://source.unsplash.com/300x200/?danmark',
+        subImage1: 'https://source.unsplash.com/90x90/?danmark',
+        subImage2: 'https://source.unsplash.com/90x90/?danmark',
+        subImage3: 'https://source.unsplash.com/90x90/?danmark'
     },
     {
         id: 4,
         cardTitle: 'Title Here',
         heading: 'Heading Here',
-        mainImage: 'https://source.unsplash.com/500x500/?danmark',
-        subImage1: 'https://source.unsplash.com/150x150/?danmark',
-        subImage2: 'https://source.unsplash.com/150x150/?danmark',
-        subImage3: 'https://source.unsplash.com/150x150/?danmark'
+        mainImage: 'https://source.unsplash.com/300x200/?danmark',
+        subImage1: 'https://source.unsplash.com/90x90/?danmark',
+        subImage2: 'https://source.unsplash.com/90x90/?danmark',
+        subImage3: 'https://source.unsplash.com/90x90/?danmark'
     },
     {
         id: 5,
         cardTitle: 'Title Here',
         heading: 'Heading Here',
-        mainImage: 'https://source.unsplash.com/1600x900/?danmark',
-        subImage1: 'https://source.unsplash.com/1600x900/?danmark',
-        subImage2: 'https://source.unsplash.com/1600x900/?danmark',
-        subImage3: 'https://source.unsplash.com/1600x900/?danmark'
+        mainImage: 'https://source.unsplash.com/300x200/?danmark',
+        subImage1: 'https://source.unsplash.com/90x90/?danmark',
+        subImage2: 'https://source.unsplash.com/90x90/?danmark',
+        subImage3: 'https://source.unsplash.com/90x90/?danmark'
     },
     {
         id: 6,
         cardTitle: 'Title Here',
         heading: 'Heading Here',
-        mainImage: 'https://source.unsplash.com/150x150/?danmark',
-        subImage1: 'https://source.unsplash.com/150x150/?danmark',
-        subImage2: 'https://source.unsplash.com/150x150/?danmark',
-        subImage3: 'https://source.unsplash.com/150x150/?danmark'
+        mainImage: 'https://source.unsplash.com/300x200/?danmark',
+        subImage1: 'https://source.unsplash.com/90x90/?danmark',
+        subImage2: 'https://source.unsplash.com/90x90/?danmark',
+        subImage3: 'https://source.unsplash.com/90x90/?danmark'
+    },
+    {
+        id: 7,
+        cardTitle: 'Title Here',
+        heading: 'Heading Here',
+        mainImage: 'https://source.unsplash.com/300x200/?danmark',
+        subImage1: 'https://source.unsplash.com/90x90/?danmark',
+        subImage2: 'https://source.unsplash.com/90x90/?danmark',
+        subImage3: 'https://source.unsplash.com/90x90/?danmark'
+    },
+    {
+        id: 8,
+        cardTitle: 'Title Here',
+        heading: 'Heading Here',
+        mainImage: 'https://source.unsplash.com/300x200/?danmark',
+        subImage1: 'https://source.unsplash.com/90x90/?danmark',
+        subImage2: 'https://source.unsplash.com/90x90/?danmark',
+        subImage3: 'https://source.unsplash.com/90x90/?danmark'
+    },
+    {
+        id: 9,
+        cardTitle: 'Title Here',
+        heading: 'Heading Here',
+        mainImage: 'https://source.unsplash.com/300x200/?danmark',
+        subImage1: 'https://source.unsplash.com/90x90/?danmark',
+        subImage2: 'https://source.unsplash.com/90x90/?danmark',
+        subImage3: 'https://source.unsplash.com/90x90/?danmark'
+    },
+    {
+        id: 10,
+        cardTitle: 'Title Here',
+        heading: 'Heading Here',
+        mainImage: 'https://source.unsplash.com/300x200/?danmark',
+        subImage1: 'https://source.unsplash.com/90x90/?danmark',
+        subImage2: 'https://source.unsplash.com/90x90/?danmark',
+        subImage3: 'https://source.unsplash.com/90x90/?danmark'
     },
 ]
 
