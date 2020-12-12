c6d6e6a3fac0a177e11fea271358fa0b092ed6d5
Shaw Malcolm <shawmalcolm@protonmail.com>
2020-12-12 14:34:32 +0000

    set up folders

---
+++ chapter-13-fetch-and-display-data/src/index.js
@@ -0,0 +1,40 @@
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import axios from "axios";
+
+class Reddit extends Component {
+  state = {
+    posts: [],
+  };
+
+  componentDidMount() {
+    axios
+      .get(`https://www.reddit.com/r/${this.props.subreddit}.json`)
+      .then((res) => {
+        const posts = res.data.data.children.map((obj) => obj.data);
+        this.setState({ posts });
+      });
+  }
+
+  render() {
+    const { posts } = this.state;
+
+    return (
+      <div>
+        <h1>{`/r/${this.props.subreddit}`}</h1>
+        <ul>
+          {posts.map((post) => (
+            <li key={post.id}>{post.title}</li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Reddit subreddit="reactjs" />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
