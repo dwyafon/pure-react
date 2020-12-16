e74594d6df864645e92d08397ec1a8a48d932ab0
Shaw Malcolm <shawmalcolm@protonmail.com>
2020-12-16 13:30:03 +0000

    update comments

--- chapter-14-update-state/src/index.js
+++ chapter-14-update-state/src/index.js
@@ -4,11 +4,21 @@ import ReactDOM from 'react-dom'
 function StepTracker() {
     const [steps, setSteps] = useState(0)
 
+    // function increment() {
+    //     setSteps(steps => steps + 1)
+    // }
+
     return (
         <div>
             Today you've taken {steps} steps!
             <br />
-            <button onClick={() => setSteps(steps => steps + 1)}>
+            {/* Could have been: */}
+            {/* <button onClick={increment}></button> */}
+            
+            {/* the parameter name does not have to be the same as the state variable name */}
+            {/*  using the functional/updater form of setSteps ensures that the latest state value is being operated on */}
+
+            <button onClick={() => setSteps(step => step + 1)}> 
                 I took another step
             </button>
         </div>
