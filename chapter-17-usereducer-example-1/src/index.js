ae5b97bf0673283a820ee2837ab460e44f609542
Shaw Malcolm <shawmalcolm@protonmail.com>
2021-01-02 06:57:34 +0000

    initial commit

---
+++ chapter-17-usereducer-example-1/src/index.js
@@ -0,0 +1,35 @@
+import React, { useReducer } from "react";
+import ReactDOM from "react-dom";
+import styled from "styled-components";
+import "./index.css";
+
+const Wrapper = styled.div`
+  height: 100vh;
+  display: flex;
+  /* justify-content: center; */
+  align-items: center;
+  flex-direction: column;
+`;
+
+const Button = styled.button`
+  background: transparent;
+  border: 1px solid #fff;
+  padding: 1rem 2rem;
+  color: #fff;
+`;
+
+const Counter = () => {
+  const [count, dispatch] = useReducer((state, action) => state + action, 0);
+
+  return (
+    <Wrapper>
+    <h1>Counter</h1>
+    <div className="count">{count}</div>
+      <Button onClick={() => dispatch(1)}>
+        +
+      </Button>
+    </Wrapper>
+  );
+};
+
+ReactDOM.render(<Counter />, document.getElementById("root"));
