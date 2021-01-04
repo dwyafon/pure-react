c7576ed6861f425ad44dc7fe79a79527ca710be0
Shaw Malcolm <shawmalcolm@protonmail.com>
2021-01-04 09:25:20 +0000

    add conditional styling

--- chapter-17-light-control/src/index.js
+++ chapter-17-light-control/src/index.js
@@ -12,37 +12,70 @@ const Wrapper = styled.div`
 `;
 
 const InnerContainer = styled.div`
-  width: 300px;
-  height: 200px;
+  width: 250px;
+  height: 250px;
   border: 1px solid #fff;
+  border-radius: 50%;
   display: flex;
   justify-content: center;
   align-items: center;
   margin: 1rem;
-`
+`;
 
 const Button = styled.button`
   background: transparent;
   border: 1px solid #fff;
-  padding: 0.5rem .75rem;
+  padding: 0.5rem 0.75rem;
   color: #fff;
-  margin-left: 0.5rem;
+`;
+
+const Bulb = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
 
 const Light = () => {
-  const [level, dispatch] = useReducer((level, value) => level + value, 0)
+  const [level, dispatch] = useReducer((level, value) => level + value, 0);
+  const style =
+    level === 0
+      ? { color: "#fff" }
+      : level === 1
+      ? {
+          color: "rgba(237, 237, 22, 0.5)",
+          border: "1px solid rgba(237, 237, 22, 0.5)",
+        }
+      : level === 2
+      ? {
+          color: "rgba(237, 237, 22, 0.75)",
+          border: "1px solid rgba(237, 237, 22, 0.75)",
+        }
+      : {
+          color: "rgba(237, 237, 22, 1)",
+          border: "1px solid rgba(237, 237, 22, 1",
+        };
 
   return (
     <Wrapper>
-    <Button onClick={ () => (level < 3 ? dispatch(1) : level) } >+</Button>
-    <InnerContainer>
-      {level === 0 ? `Off` :  level === 1 ? `Low` : level === 2 ? `Medium` : `High`}
-    </InnerContainer>
-    <Button onClick={ () => (level > 0 ? dispatch(-1) : level) }>-</Button>
-    <Button style={{marginTop: 18}} onClick={ () => dispatch(-level) }>OFF</Button>
-  </Wrapper>
-  )
-}
-
+      <Bulb>
+        <Button onClick={() => (level > 0 ? dispatch(-1) : level)}>-</Button>
+        <InnerContainer style={style}>
+          {level === 0
+            ? `off`
+            : level === 1
+            ? `low`
+            : level === 2
+            ? `medium`
+            : `high`}
+        </InnerContainer>
+        <Button onClick={() => (level < 3 ? dispatch(1) : level)}>+</Button>
+      </Bulb>
+        <Button style={{ marginTop: 18 }} onClick={() => dispatch(-level)}>
+          OFF
+        </Button>
+    
+    </Wrapper>
+  );
+};
 
 ReactDOM.render(<Light />, document.getElementById("root"));
