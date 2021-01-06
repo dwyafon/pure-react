62a8cb9b824ff3886b111e55d554c8688b65af6b
Shaw Malcolm <shawmalcolm@protonmail.com>
2021-01-06 12:38:09 +0000

    added input

--- chapter-18-fetch-with-useeffect/src/index.js
+++ chapter-18-fetch-with-useeffect/src/index.js
@@ -1,15 +1,34 @@
 import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
 
-function Reddit() {
+function App() {
+  const [inputValue, setValue] = useState('reactjs');
+  const [subreddit, setSubreddit] = useState(inputValue);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setSubreddit(inputValue);
+  };
+
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <input value={inputValue} onChange={(e) => setValue(e.target.value)} />
+      </form>
+      <Reddit subreddit={subreddit} />
+    </>
+  );
+}
+
+function Reddit({ subreddit }) {
   const [posts, setPosts] = useState([]);
 
   useEffect(() => {
-    fetch('https://www.reddit.com/r/reactjs.json') // Getting network error in Firefox; works in 'Chrome'
+    fetch(`https://www.reddit.com/r/${subreddit}.json`) // Getting network error in Firefox; works in 'Chrome'
       .then((res) => res.json())
-      .then((json) => setPosts(json.data.children.map((c) => c.data))) 
-      .catch(error => console.log(error))
-  }, [setPosts]);
+      .then((json) => setPosts(json.data.children.map((c) => c.data)))
+      .catch((error) => console.log(error));
+  }, [subreddit, setPosts]);
 
   return (
     <ul>
@@ -20,4 +39,4 @@ function Reddit() {
   );
 }
 
-ReactDOM.render(<Reddit />, document.querySelector('#root'));
+ReactDOM.render(<App />, document.querySelector('#root'));
