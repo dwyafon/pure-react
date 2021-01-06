06f6559fd80e4ec1693bac424a6882300e6a2f96
Shaw Malcolm <shawmalcolm@protonmail.com>
2021-01-06 08:13:33 +0000

    initial commit

---
+++ chapter-18-useeffect-hook/src/index.js
@@ -0,0 +1,16 @@
+import React, { useState, useEffect } from 'react';
+import ReactDOM from 'react-dom'
+
+const LogEffect = () => {
+    const [text, setText] = useState('');
+
+    useEffect(() => {
+        console.log("latest value:", text)
+    });
+
+    return (
+        <input value={text} onChange={e => setText(e.target.value)} />
+    );
+};
+
+ReactDOM.render(<LogEffect />, document.querySelector("#root"))
