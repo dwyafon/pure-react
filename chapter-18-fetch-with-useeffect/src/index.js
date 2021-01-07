fe1f9618f3942196a92d90877c07d20b606b7cf7
Shaw Malcolm <shawmalcolm@protonmail.com>
2021-01-07 18:07:27 +0000

    updated error handling

--- chapter-18-fetch-with-useeffect/src/index.js
+++ chapter-18-fetch-with-useeffect/src/index.js
@@ -1,23 +1,48 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import ReactDOM from 'react-dom';
+import styled from 'styled-components';
+
+const Input = styled.input`
+  padding: 1rem 2rem;
+  font-size: 1.5rem;
+`;
+const Button = styled.button`
+  padding: 0.5rem 1rem;
+  margin-left: 0.5rem;
+`;
+
+const Error = styled.div`
+  font-size: 5rem;
+  font-family: monospace;
+  color: red;
+`;
 
 function App() {
   const [inputValue, setValue] = useState('reactjs');
   const [subreddit, setSubreddit] = useState(inputValue);
 
+  const input = useRef();
+
   const handleSubmit = (e) => {
     e.preventDefault();
     setSubreddit(inputValue);
+    setValue('');
   };
 
   useEffect(() => {
-    document.title = subreddit; //using `inputValue` instead of `subreddit` would result in the title changing in the tab as the input value changes
-  }, [subreddit])
+    document.title = subreddit;
+    input.current.focus();
+  }, [subreddit, input]);
 
   return (
     <>
       <form onSubmit={handleSubmit}>
-        <input value={inputValue} onChange={(e) => setValue(e.target.value)} />
+        <Input
+          ref={input}
+          value={inputValue}
+          onChange={(e) => setValue(e.target.value)}
+        />
+        <Button onClick={handleSubmit}>Submit</Button>
       </form>
       <Reddit subreddit={subreddit} />
     </>
@@ -26,20 +51,32 @@ function App() {
 
 function Reddit({ subreddit }) {
   const [posts, setPosts] = useState([]);
+  const [error, setError] = useState(false);
+
+  const handleError = () => {
+    setError(true);
+    setPosts([]);
+  };
 
   useEffect(() => {
-    fetch(`https://www.reddit.com/r/${subreddit}.json`) // Getting network error in Firefox; works in 'Chrome'
+    setError(false);
+    fetch(`https://www.reddit.com/r/${subreddit}.json`)
       .then((res) => res.json())
       .then((json) => setPosts(json.data.children.map((c) => c.data)))
-      .catch((error) => console.log(error));
+      .catch((error) => handleError());
   }, [subreddit, setPosts]);
 
   return (
-    <ul>
-      {posts.map((post) => (
-        <li key={post.id}>{post.title}</li>
-      ))}
-    </ul>
+    <>
+      {error && (
+        <Error className='error'>Please enter an existing subreddit name</Error>
+      )}
+      <ul>
+        {posts.map((post) => (
+          <li key={post.id}>{post.title}</li>
+        ))}
+      </ul>
+    </>
   );
 }
 
