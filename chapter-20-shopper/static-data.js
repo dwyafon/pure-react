b04062f4b3f5d6b0df92df7a92cfeb4637bafd4b
Shaw Malcolm <shawmalcolm@protonmail.com>
2021-01-09 15:27:15 +0000

    add items array

---
+++ chapter-20-shopper/static-data.js
@@ -0,0 +1,34 @@
+const items = [
+    {
+        id: 0,
+        name: "Apple iPad",
+        description: "An iPad like no other. WiFi, 4G, lots of storage",
+        price: 329.00
+    },
+    {
+        id: 1,
+        name: "Apple iPad Pro",
+        description: "Even more expensive than the regular iPad",
+        price: 799.00
+    },
+    {
+        id: 2,
+        name: "Canon T7i",
+        description: "DSLY with lots of bells and whistles",
+        price: 749.99
+    },
+    {
+        id: 3,
+        name: "Apple Watch",
+        description: "Tells the time. Does other stuff,too",
+        price: 249.99
+    },
+    {
+        id: 4,
+        name: "Apple Watch Silver",
+        description: "An even more expensive watch",
+        price: 599.00
+    },
+]
+
+export {items}
